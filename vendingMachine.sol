d9b60f10a7e94d66b1d7aceffc7b195afa8a0044
io-pankaj <104295818+io-pankaj@users.noreply.github.com>
2022-04-25 16:27:10 +0545

    Add files via upload

---
+++ vendingMachine.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract vendingMachine {
+    address public owner;
+    mapping ( address => uint ) public donutsBalances;
+
+    constructor(){
+        owner = msg.sender;
+        donutsBalances[address(this)]=100;
+    }
+    function getVendingMachineBalances() public view returns (uint ){
+        return donutsBalances[address(this)];
+    }
+    function restock(uint amount) public{
+        require(msg.sender==owner,"only the owner can restock this machine");
+        donutsBalances[address(this)]+= amount;
+    }
+    function purchase(uint amount) public payable {
+        // require(msg.value >= amount*  , "you must pay atleast 2 ether");
+        require(donutsBalances[address(this)]>= amount,"not enough donuts in stock.");
+        donutsBalances[address(this)]-=amount;
+        donutsBalances[msg.sender]+= amount;
+    }
+}
